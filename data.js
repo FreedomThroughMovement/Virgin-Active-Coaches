0fc0cdb3139736ba64e1bf54bedd4b5a10896e9e
FreedomThroughMovement <kenchungcoaching@gmail.com>
2026-07-25 10:00:57 +0800

    Update coach image filename in data.js

--- data.js
+++ data.js
@@ -56,7 +56,7 @@ const coaches = [
         specialty: "Coach",
         bio: "Stronger. Healthier. Confident. I'll guide you to move better, perform better and live stronger.",
         focus: ["body-transformation", "strength-performance", "race-preparation", "senior-training", "nutrition", "sports-performance"],
-        image: "image/Faiz.jpg",
+        image: "image/Ryan.jpg",
         experience: "2+ years",
         certifications: ["ACE CPT", "ACE Senior Fitness Specialist", "ACE Weight Management Specialist", "ACE Fitness Nutrition Specialist", "ACE Corrective Exercise Specialist", "Evolt 360 Specialist"]
     },
